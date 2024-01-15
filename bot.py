5c84b26fd933a39ad0bab8af9a2cf38aaae968a5
lis531 <82408934+lis531@users.noreply.github.com>
2024-01-15 22:29:25 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 
 BOT_TOKEN = "MTA4MjczOTkyNDAyMjg2MTg1Ng.Gtg1cD.QIgWEVho2SGOvH_-WEyL8-qPeFPvkCUVwudvQY"
 
@@ -13,5 +13,23 @@ async def on_ready():
 async def on_message(message):
     if bot.user.mentioned_in(message):
         await message.channel.send("SHUT THE FUCK UP")
+
+jajco_count = {}
+
+@bot.hybrid_command()
+async def jajco(ctx, user: discord.User):
+    if not user:
+        await ctx.send("Wprowadź nazwę użytkownika")
+        return
+    user_id = str(user.id)
+    jajco_count[user_id] = jajco_count.get(user_id, 0) + 1
+    await ctx.send(f"{user.mention} ilość jajec to {jajco_count[user_id]}.")
+
+async def jajcoCheck(ctx, user: discord.User):
+    if not user:
+        await ctx.send("Wprowadź nazwę użytkownika")
+        return
+    user_id = str(user.id)
+    await ctx.send(f"{user.mention} ilość jajec to {jajco_count[user_id]}.")
 
 bot.run(BOT_TOKEN)
